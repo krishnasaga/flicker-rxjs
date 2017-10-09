b8c4b49061a0780690b6df2351f7aa3ab69d3e02
Krishna Sagar R <krishnasagar.r@sonata-software.com>
2017-10-10 00:55:26 +0100

    feature

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ Observable.fromEvent(input, "keyup")
   .debounceTime(500)
   .mergeMap(event =>
     Observable.ajax({
-      url: `tps://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f188cbce9e935de505e47517e491f0bf&text=hello&format=json&nojsoncallback=1&auth_token=72157661326245188-dd65d256be432cb9&api_sig=596dac512c6fbc6e436a25ddc59fe932`,
+      url: `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f188cbce9e935de505e47517e491f0bf&text=hello&format=json&nojsoncallback=1&auth_token=72157661326245188-dd65d256be432cb9&api_sig=596dac512c6fbc6e436a25ddc59fe932`,
       responseType: "json"
     }).catch(err => (err.xhr ? Observable.of(err) : Observable.of(".___.")))
   )
